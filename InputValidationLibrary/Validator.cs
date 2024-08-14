75bd41a2e041f4a443f2ff26647ecd40ef9caa3f
YusufAlZadid <139882679+YusufAlZadid@users.noreply.github.com>
2024-08-14 16:40:57 +1000

    Task1.2&1.3

---
+++ InputValidationLibrary/Validator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputValidationLibrary
+{
+    public static class Validator
+    {
+        // Method to validate if the input is a valid integer
+        public static bool IsValidInteger(string input, out int result)
+        {
+            return int.TryParse(input, out result);
+        }
+
+        // Method to validate if the input is a valid double
+        public static bool IsValidDouble(string input, out double result)
+        {
+            return double.TryParse(input, out result);
+        }
+
+        // Method to validate if the input is a non-empty string
+        public static bool IsValidString(string input)
+        {
+            return !string.IsNullOrWhiteSpace(input);
+        }
+    }
+}
---
+++ Task1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathOperations;
+using InputValidationLibrary;
+
+namespace Task1
+{
+    public partial class Form1 : Form
+    {
+        private Calculator calculator;
+
+        public Form1()
+        {
+            InitializeComponent();
+            calculator = new Calculator();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Validator.IsValidInteger(textBox1.Text, out int number1) &&
+                Validator.IsValidInteger(textBox2.Text, out int number2))
+            {
+                // Perform addition
+                int result = number1 + number2;
+                textBox3.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid integers.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Validator.IsValidInteger(textBox1.Text, out int number1) &&
+                Validator.IsValidInteger(textBox2.Text, out int number2))
+            {
+                // Perform subtraction
+                int result = number1 - number2;
+                textBox3.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid integers.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        /*private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }*/
+
+
+
+    }
+}
